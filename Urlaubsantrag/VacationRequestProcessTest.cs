d04c89f3c1a25e9f174970aa6c516c6092192db8
Bernd Rücker <bernd.ruecker@camunda.com>
2017-02-17 11:11:07 +0100

    added simple example for Basta

---
+++ Urlaubsantrag/VacationRequestProcessTest.cs
@@ -0,0 +1,68 @@
+﻿using CamundaClient;
+using CamundaClient.Service;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using CamundaClient.Dto;
+
+namespace VacationRequestProcess
+{
+    [TestFixture]
+    class CalculationProcessTest
+    {
+        [Test]
+        public void TestHappyPath()
+        {
+            // Engine client should point to a dedicated Camunda instance for test, preferrably locally available
+            var camunda = new CamundaEngineClient(new System.Uri("http://localhost:8080/engine-rest/engine/default/"), null, null);
+
+            // Deploy the process under test
+            string deploymentId = camunda.RepositoryService.Deploy("testcase", new List<object> {
+                FileParameter.FromManifestResource(Assembly.GetExecutingAssembly(), "Urlaubsantrag.Models.Urlaubsantrag.bpmn") });
+            try
+            {
+
+                string processInstanceId = camunda.BpmnWorkflowService.StartProcessInstance("Urlaubsantrag", new Dictionary<string, object>()
+                {
+                    {"name", "Bernd Rücker" },
+                    {"vacationStartDate", 10 },
+                    {"duration", 5 }
+                });
+
+                var tasks = camunda.HumanTaskService.LoadTasks(new Dictionary<string, string>() {
+                    { "processInstanceId", processInstanceId }
+                });
+                Assert.AreEqual(1, tasks.Count);
+                Assert.AreEqual("UserTask_UrlaubGenehmigen", tasks.First().TaskDefinitionKey);
+
+                camunda.HumanTaskService.Complete(tasks.First().Id, new Dictionary<string, object>()
+                {
+                    {"approved", true }
+                });
+
+
+                var externalTasks = camunda.ExternalTaskService.FetchAndLockTasks("testcase", 100, "leaveAccount", 1000, new List<string>() { "name" });
+                Assert.AreEqual(1, externalTasks.Count);
+                Assert.AreEqual("ServiceTask_UrlaubstageAbbuchen", externalTasks.First().ActivityId);
+                Assert.AreEqual("Bernd Rücker", externalTasks.First().Variables["name"].Value);
+
+                camunda.ExternalTaskService.Complete("testcase", externalTasks.First().Id, new Dictionary<string, object>());
+
+                // TODO: Simulate timer
+                //externalTasks = camunda.ExternalTaskService.FetchAndLockTasks("testcase", 100, "informColleagues", 1000, new List<string>());
+                //Assert.AreEqual(1, externalTasks.Count);
+                //Assert.AreEqual("ServiceTask_KollegenInformieren", externalTasks.First().ActivityId);
+                //camunda.ExternalTaskService.Complete("testcase", externalTasks.First().Id, new Dictionary<string, object>());
+
+                // not the process instance has ended, TODO: Check state with History
+            }
+            finally
+            {
+                // cleanup after test case
+                camunda.RepositoryService.DeleteDeployment(deploymentId);
+            }
+        }
+    }
+}
